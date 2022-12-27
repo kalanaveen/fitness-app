94de37b5d9f1822bf561a0f5820eaeda6ae0b732
naveen kala <kalanaveen55@gmail.com>
2022-12-27 16:08:27 +0530

    layout ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { Box } from '@mui/material';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+
+
 
 function App() {
   return (
-    <div>
-        app
-    </div>
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
+      <Navbar />
+      <Routes>
+        <Route to="/" element={ <Home/>} />
+        <Route to="/exercise/:id" element={ <ExerciseDetail/>}/>
+      </Routes>
+      <Footer/>
+      </Box>
   );
 }
 
---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExerciseDetail = () => {
+  return (
+    <div>
+      excersise detail
+    </div>
+  )
+}
+
+export default ExerciseDetail
